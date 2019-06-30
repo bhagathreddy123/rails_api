e8a666d052a6e70e0aaf2cb91e69be4abd09e7de
Bhagath Reddy <katta04.bhagath2gmail.com>
2019-06-30 18:39:37 +0530

    log out endpoint for user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
   before_action :load_user,  only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
+
 	def create
 		if @user.valid_password?(sign_in_params[:password])
 			sign_in "user", @user
@@ -9,6 +12,13 @@ class Api::V1::SessionsController < Devise::SessionsController
             json_response "Un authorized", false,  {}, :unauthorized
 		end
 	end
+
+	def destroy
+		sign_out @user
+		@user.generate_new_access_token
+		json_response "Log out successfully", true, {}, :ok
+	end
+
 
     private
 
@@ -24,4 +34,15 @@ class Api::V1::SessionsController < Devise::SessionsController
 			json_response "Cannot get user", false, {}, :failure
 		end
 	end
+
+	def valid_token
+		@user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+		if @user
+			return @user
+		else
+			json_response "Invalid Token", false, {}, :failure
+		end
+	end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   acts_as_token_authenticatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def generate_new_access_token
+  	token = User.generate_unique_secure_token
+  	update_attributes authentication_token: token
+  end
 end
+
